b47c7635056285055646c7336bcf78a907e60666
srisridasari <166973445+srisridasari@users.noreply.github.com>
2024-04-14 17:25:02 +0530

    Handle KeyboardInterrupt error to exit gracefully
    #1  Commit changes successfully in main.py

--- main.py
+++ main.py
@@ -22,20 +22,20 @@ def main():
         # print(box_idx, f, n)
         print(q)
         print("-" * 4)
-        input("Answer: ")
-        o = input(f"The answer was: {a}\nWere you can correct? (Y/n/exit): ")
-        print("=" * 5)
-        if not o or o[0].lower() == "y":
+        user_answer = input("Answer: ")
+        print(f"The answer was: {a}")
+        if user_answer.lower() == a.lower():
+            print("You were correct!")
             box_idx = min(box_idx + 1, len(slots) - 1)
-        elif o[0].lower() == "n":
-            box_idx = max(box_idx - 1, 0)
         else:
-            break
+            print("You were incorrect.")
+            box_idx = max(box_idx - 1, 0)
+        print("=" * 5)
         slots[box_idx].append((q, a))
         if len(cards) == len(slots[-1]):
-            print(f"You have memorised all {len(cards)} cards")
-            k = input("Exit? (N/y): ")
-            if o and o[0].lower() == "y":
+            print(f"You have memorized all {len(cards)} cards")
+            exit_choice = input("Exit? (Y/n): ")
+            if exit_choice.lower() == "y":
                 break
 
 if __name__ == "__main__":
